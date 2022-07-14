64bf3e989a005773aa9b2b5bce4ef138733660a6
Ives Moreira Gomes <ives.gomes@acensjr.com>
2022-07-14 20:59:01 -0300

    feat: create kafka topic, producer and consumer

--- ms-emails/src/app.ts
+++ ms-emails/src/app.ts
@@ -1,37 +1,63 @@
 import express from 'express';
 import cors from 'cors';
 import nodemailer from 'nodemailer';
+import { Kafka } from 'kafkajs';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 const port = 3334;
 
-app.get('/', (req, res) => {
-	const transport = nodemailer.createTransport({
-		host: "smtp.mailtrap.io",
-		port: 2525,
-		auth: {
-			user: "0d65f13fede99c",
-			pass: "750e45bf5e202a"
-		}
-	});
+app.get('/', async (req, res) => {
+	const kafka = new Kafka({
+		clientId: 'ms-emails',
+		brokers: ['localhost:9092'],
+	})
 
-	const mailOptions = {
-		from: 'no-reply@ms-emails.com',
-		to: 'sender@email.com',
-		subject: 'E-mail enviado usando Node!',
-		html: '<p>Bem fácil, não? ;)</p>'
-	};
+	const admin = kafka.admin()
+	const consumer = kafka.consumer({ groupId: '1' }) // verify group
 
-	transport.sendMail(mailOptions, function (error, info) {
-		if (error) {
-			console.log(error);
-			return res.status(400).send({ message: 'Falha no envio do email' })
-		} else {
-			console.log('Email enviado: ' + info.response);
-		}
-	});
+	await admin.connect()
+	await consumer.connect()
+
+	await consumer.subscribe({ topic: 'kafka-topic', fromBeginning: true })
+
+	await consumer.run({
+		eachMessage: async ({ topic, partition, message }) => {
+			console.log({
+				value: message.value?.toString(),
+			})
+		},
+	})
+
+	await admin.disconnect()
+
+	return res.send()
+
+	// const transport = nodemailer.createTransport({
+	// 	host: "smtp.mailtrap.io",
+	// 	port: 2525,
+	// 	auth: {
+	// 		user: "0d65f13fede99c",
+	// 		pass: "750e45bf5e202a"
+	// 	}
+	// });
+
+	// const mailOptions = {
+	// 	from: 'no-reply@ms-emails.com',
+	// 	to: 'sender@email.com',
+	// 	subject: 'E-mail enviado usando Node!',
+	// 	html: '<p>Bem fácil, não? ;)</p>'
+	// };
+
+	// transport.sendMail(mailOptions, function (error, info) {
+	// 	if (error) {
+	// 		console.log(error);
+	// 		return res.status(400).send({ message: 'Falha no envio do email' })
+	// 	} else {
+	// 		console.log('Email enviado: ' + info.response);
+	// 	}
+	// });
 
 	return res.status(200).send({ message: 'E-mail enviado com sucesso!' })
 });
